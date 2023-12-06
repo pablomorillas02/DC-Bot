b38d15e70c12ee7ad4b9588ed82d84f30fdf09c0
pablomorillas02 <pmorillasplaza.pmp@gmail.com>
2023-12-06 17:27:11 +0100

    Lógica chistes
    Se ha añadido la lógica temporal para mandar mensajes.

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,7 @@
+import asyncio
 import discord
-import respuestas      
+import respuestas   
+import chistes   
 
 # Esta función obtiene el token desde un fichero de texto externo 
 def get_token() -> str:
@@ -16,13 +18,30 @@ def run_discord_bot():
     intents.message_content = True
     client = discord.Client(intents=intents)
     
-    print(f'Poniendo en marcha... ({TOKEN})')
+    print(f'Poniendo en marcha... {TOKEN}')
     
     # Lógica del bot debajo
+    
+    # Este trozo de código espera un tiempo determinado para contar un chiste
+    async def send_joke():
+        time = 2 # tiempo en segundos
+        channel_id = 1181912230871191575 # esto hay que ir cambiándolo en función del canal
+        channel = client.get_channel(channel_id)
+        
+        while True: # Este bucle espera el tiempo estipulado
+            chiste = await chistes.get_joke()
+        
+            if channel:
+                await channel.send(chiste)
+                
+            await asyncio.sleep(time)
     
     @client.event
     async def on_ready():
         print(f'{client.user} está funcionando')
+        
+        # loop para el envío de chistes
+        client.loop.create_task(send_joke())
     
     @client.event
     async def on_message(message):
---
+++ bot/chistes.py
@@ -0,0 +1,4 @@
+# Este módulo temporiza el tiempo para mandar chistes, además de elegirlos de un fichero de texto de manera pseudoaleatoria
+
+async def get_joke() -> str:
+    return 'chiste'
